a3738f22c8dc4225206d19a58ebc648925b2f99f
Aleksandr Kabanskiy <alexander.777.kr@gmail.com>
2020-04-01 17:44:24 +0300

    Updates

---
+++ precache-manifest.e7a2a97e3645b734d889eaa1a34c548f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828f79c769925b6ff48771d7dc2fa509",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "aa3fe5c92f8e5f56f59e",
+    "url": "/monsters-rolodex/static/css/main.0a6c3132.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3fe5c92f8e5f56f59e",
+    "url": "/monsters-rolodex/static/js/main.8fd804ce.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
